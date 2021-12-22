621c59016793ebc3331162cd637ce2387091576c
Tim Osborn <tim@memelab.com.au>
2021-12-22 15:28:06 +1100

    Update to latest miniflare
    …and version bumps, but:
    
    - requests under `miniflare dev` and
    - test still fail

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,15 @@ import { Router } from "itty-router";
 
 const router = Router();
 
+/**
+  [mf:err] GET /: TypeError: Failed to parse URL from [object Object]
+
+    at new Request (/Users/ptim/Documents/Projects/New-Atlas/source/miniflare-typescript-esbuild-jest/node_modules/undici/lib/fetch/request.js:83:23)
+    at new Request (/Users/ptim/Documents/Projects/New-Atlas/source/miniflare-typescript-esbuild-jest/node_modules/miniflare/node_modules/@miniflare/core/src/standards/http.ts:348:13)
+    at EventTarget.dispatchFetch (/Users/ptim/Documents/Projects/New-Atlas/source/miniflare-typescript-esbuild-jest/node_modules/miniflare/node_modules/@miniflare/core/src/index.ts:894:51)
+    at processTicksAndRejections (node:internal/process/task_queues:96:5)
+    at Server.<anonymous> (/Users/ptim/Documents/Projects/New-Atlas/source/miniflare-typescript-esbuild-jest/node_modules/@miniflare/http-server/src/index.ts:179:20)
+*/
 export async function handleRequest(request: Request, env: Bindings) {
   router.all("*", () => new Response("OK", { status: 200 }));
   return router.handle(request, env);
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,7 +1,13 @@
-// SyntaxError: The requested module 'itty-router' does not provide an export named 'Router'
-// at Runtime.linkAndEvaluateModule (node_modules/jest-runtime/build/index.js:779:5)
 import { handleRequest } from "@/index";
 
+/**
+  FAIL  test/index.spec.ts
+  ● Test suite failed to run
+
+    SyntaxError: The requested module 'itty-router' does not provide an export named 'Router'
+
+      at Runtime.linkAndEvaluateModule (node_modules/jest-runtime/build/index.js:779:5)
+ */
 test("should redirect to example page on no route match", async () => {
   const env = getMiniflareBindings();
   const res = await handleRequest(new Request("http://localhost"), env);
