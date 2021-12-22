341f3e46ca4961336e6bd64461211d8df7fbd10e
Tim Osborn <tim@memelab.com.au>
2021-12-22 14:48:36 +1100

    Add test to repro jest spy error
    ```
     FAIL  test/response.spec.ts
      ● should spy on buildResponse
    
        expect(received).toHaveBeenCalledTimes(expected)
    
        Matcher error: received value must be a mock or spy function
    
        Received has type:  function
        Received has value: [Function buildResponse]
    
          17 |
          18 | test("should spy on buildResponse", async () => {
        > 19 |   expect(buildResponse).toHaveBeenCalledTimes(0);
             |                         ^
          20 |
          21 |   const res = buildResponse("not found", 404);
          22 |   expect(res.status).toBe(404);
    
          at ensureMockOrSpy (node_modules/expect/build/spyMatchers.js:1294:11)
          at Object.<anonymous> (test/response.spec.ts:19:25)
    ```

--- test/response.spec.ts
+++ test/response.spec.ts
@@ -1,4 +1,7 @@
 import { buildResponse } from "@/response";
+import { jest } from "@jest/globals";
+
+jest.mock("@/response");
 
 test("should build response", async () => {
   const res = buildResponse("text");
@@ -10,4 +13,70 @@ test("should build response", async () => {
 test("should build response with custom status", () => {
   const res = buildResponse("not found", 404);
   expect(res.status).toBe(404);
+});
+
+/**
+ ● should spy on buildResponse
+
+ expect(received).toHaveBeenCalledTimes(expected)
+
+ Matcher error: received value must be a mock or spy function
+
+ Received has type:  function
+ Received has value: [Function buildResponse]
+
+   17 |
+   18 | test("should spy on buildResponse", async () => {
+ > 19 |   expect(buildResponse).toHaveBeenCalledTimes(0);
+      |                         ^
+   20 |
+   21 |   const res = buildResponse("not found", 404);
+   22 |   expect(res.status).toBe(404);
+
+   at ensureMockOrSpy (node_modules/expect/build/spyMatchers.js:1294:11)
+   at Object.<anonymous> (test/response.spec.ts:19:25)
+*/
+test("should spy on buildResponse", async () => {
+  expect(buildResponse).toHaveBeenCalledTimes(0);
+
+  const res = buildResponse("not found", 404);
+  expect(res.status).toBe(404);
+
+  expect(buildResponse).toHaveBeenCalledTimes(1);
+});
+
+/**
+  With proper typing, eg:
+  https://www.benmvp.com/blog/using-jest-mock-functions-typescript/
+
+  ● should spy with typed mock
+
+  expect(received).toHaveBeenCalledTimes(expected)
+
+  Matcher error: received value must be a mock or spy function
+
+  Received has type:  function
+  Received has value: [Function buildResponse]
+
+    33 |     typeof buildResponse
+    34 |   >;
+  > 35 |   expect(mockBuildResponse).toHaveBeenCalledTimes(0);
+       |                             ^
+    36 |
+    37 |   const res = mockBuildResponse("not found", 404);
+    38 |   expect(res.status).toBe(404);
+
+    at ensureMockOrSpy (node_modules/expect/build/spyMatchers.js:1294:11)
+    at Object.<anonymous> (test/response.spec.ts:35:29)
+ */
+test("should spy with typed mock", async () => {
+  const mockBuildResponse = buildResponse as jest.MockedFunction<
+    typeof buildResponse
+  >;
+  expect(mockBuildResponse).toHaveBeenCalledTimes(0);
+
+  const res = mockBuildResponse("not found", 404);
+  expect(res.status).toBe(404);
+
+  expect(mockBuildResponse).toHaveBeenCalledTimes(1);
 });
