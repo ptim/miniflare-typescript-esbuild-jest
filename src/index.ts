21dd7ad24483c9b84f3f4c79bf03a68abb96d86d
Tim Osborn <tim@memelab.com.au>
2022-03-30 19:31:15 +1100

    Example injecting a meta tag using content from remote API

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,44 @@
-export async function handleRequest(request: Request, env: Bindings) {
-  // Match route against pattern /:name/*action
-  const url = new URL(request.url);
-  const match = /\/(?<name>[^/]+)(?<action>.*)/.exec(url.pathname);
-  if (!match?.groups) {
-    // If we didn't specify a name, default to "test"
-    return Response.redirect(`${url.origin}/test/increment`, 302);
-  }
+const createMetaTag = async () => {
+  const res = await fetch(
+    "https://www.timeapi.io/api/Time/current/zone?timeZone=Australia/Melbourne"
+  );
+  const json: any = await res.json();
+  const metaTag = `<meta name="cf-injected-time" content="${json?.dateTime}">`;
+  console.log("Look for the meta tag in the iframe head in devtools 👍", {
+    json,
+  });
+  return metaTag;
+};
 
-  // Forward the request to the named Durable Object...
-  const { COUNTER } = env;
-  const id = COUNTER.idFromName(match.groups.name);
-  const stub = COUNTER.get(id);
-  // ...removing the name prefix from URL
-  url.pathname = match.groups.action;
-  return stub.fetch(url.toString());
+class HeadElementHandler implements HTMLRewriterElementContentHandlers {
+  async element(el: Element) {
+    el.prepend(await createMetaTag(), { html: true });
+  }
+}
+
+/**
+ * @see https://developers.cloudflare.com/workers/runtime-apis/html-rewriter/#element-handlers
+ * @see https://developers.cloudflare.com/workers/examples/rewrite-links/
+ */
+export async function handleRequest(request: Request, env: Bindings) {
+  const res = await fetch("https://en.wikipedia.org/");
+
+  /**
+   * Loading homepage on clientside spawns a bunch of other request that this worker
+   * will also handle - return early if it's not a homepage request
+   */
+  const isHomepage = new URL(request.url).pathname === "/";
+  if (!isHomepage) return res;
+
+  // If the response is HTML, it can be transformed with
+  // HTMLRewriter -- otherwise, it should pass through
+  const contentType = res.headers.get("Content-Type");
+  if (contentType?.startsWith("text/html")) {
+    const rewriter = new HTMLRewriter().on("head", new HeadElementHandler());
+    return rewriter.transform(res);
+  } else {
+    return res;
+  }
 }
 
 const worker: ExportedHandler<Bindings> = { fetch: handleRequest };
