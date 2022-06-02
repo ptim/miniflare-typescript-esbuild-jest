ae8ddde1880771c11f99339cef916b7a2ad92168
Tim Osborn <tim@memelab.com.au>
2022-06-02 17:50:59 +1000

    Add honeycomb logging, with caught error in testing

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,37 @@
+import {
+  ResolvedConfig,
+  wrapModule,
+} from "@cloudflare/workers-honeycomb-logger";
+
 export async function handleRequest(request: Request, env: Bindings) {
   return Response.redirect("http://redirected.dev", 302);
 }
 
-const worker: ExportedHandler<Bindings> = { fetch: handleRequest };
-export default worker;
+const handler = {
+  fetch: handleRequest as ExportedHandlerFetchHandler<Bindings>,
+};
+
+/** https://github.com/cloudflare/workers-honeycomb-logger */
+const honeycombConfig: Partial<ResolvedConfig> = {
+  dataset: "test-handler",
+  /** https://github.com/cloudflare/workers-honeycomb-logger/#dynamic-sampling */
+  sampleRates(data: any): number {
+    try {
+      console.log(data.response.headers.get?.("x-A")); // safe due to optional chaining
+      console.log(data.response.headers.get("x-B")); // throws
+    } catch (error) {
+      // TypeError: data.response.headers.get is not a function
+      // at sampleRates (~/miniflare-typescript-esbuild-jest/src/index.ts:21:41)
+      console.error(error);
+      /*
+      typeof data.response.headers
+      'object'
+
+      data.response.headers instanceof Headers
+      false
+      */
+    }
+    return 1;
+  },
+};
+export default wrapModule(honeycombConfig, handler);
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,11 +1,17 @@
 import { Miniflare } from "miniflare";
+import { handleRequest } from "@/index";
 
 test("should redirect", async () => {
+  const env = getMiniflareBindings();
+  const res = await handleRequest(new Request("http://localhost"), env);
+  expect(res.headers.get("location")).toEqual("http://redirected.dev/");
+});
+
+test("should redirect (using mf.dispatchFetch)", async () => {
   const mf = new Miniflare({
     scriptPath: "dist/index.mjs",
     modules: true,
   });
-
-  const result = await mf.dispatchFetch("http://localhost/");
-  expect(result.headers.get("location")).toEqual("http://redirected.dev/");
+  const res = await mf.dispatchFetch("http://localhost/");
+  expect(res.headers.get("location")).toEqual("http://redirected.dev/");
 });
